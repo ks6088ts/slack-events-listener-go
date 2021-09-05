b958ba1c4e9b3f957ee700f30083b797c662d8f0
ks6088ts <ks6088ts@gmail.com>
2021-09-05 17:55:05 +0900

    implement run command

--- cli/cmd/run.go
+++ cli/cmd/run.go
@@ -22,36 +22,117 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
 
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 	"github.com/spf13/cobra"
+)
+
+type runOptions struct {
+	slackSigningSecret string
+	slackBotToken      string
+	port               int
+}
+
+var (
+	o = &runOptions{}
 )
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Run a slack event listener",
+	Long:  `Run a slack event listener`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		api := slack.New(o.slackBotToken)
+
+		http.HandleFunc("/slack/events", func(w http.ResponseWriter, r *http.Request) {
+			verifier, err := slack.NewSecretsVerifier(r.Header, o.slackSigningSecret)
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			bodyReader := io.TeeReader(r.Body, &verifier)
+			body, err := ioutil.ReadAll(bodyReader)
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			if err := verifier.Ensure(); err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+			if err != nil {
+				log.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			switch eventsAPIEvent.Type {
+			case slackevents.URLVerification:
+				var res *slackevents.ChallengeResponse
+				if err := json.Unmarshal(body, &res); err != nil {
+					log.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "text/plain")
+				if _, err := w.Write([]byte(res.Challenge)); err != nil {
+					log.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+			case slackevents.CallbackEvent:
+				innerEvent := eventsAPIEvent.InnerEvent
+				switch event := innerEvent.Data.(type) {
+				case *slackevents.AppMentionEvent:
+					message := strings.Split(event.Text, " ")
+					if len(message) < 2 {
+						w.WriteHeader(http.StatusBadRequest)
+						return
+					}
+
+					command := message[1]
+					switch command {
+					case "ping":
+						if _, _, err := api.PostMessage(event.Channel, slack.MsgOptionText("pong", false)); err != nil {
+							log.Println(err)
+							w.WriteHeader(http.StatusInternalServerError)
+							return
+						}
+					}
+				}
+			}
+		})
+
+		log.Println("[INFO] Server listening")
+		if err := http.ListenAndServe(":"+strconv.Itoa(o.port), nil); err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
 
-	// Here you will define your flags and configuration settings.
+	runCmd.Flags().IntVarP(&o.port, "port", "p", 8080, "port number")
+	runCmd.Flags().StringVarP(&o.slackSigningSecret, "secret", "s", "default", "slack signing secret")
+	runCmd.Flags().StringVarP(&o.slackBotToken, "token", "t", "default", "slack bot token")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	runCmd.MarkFlagRequired("secret")
+	runCmd.MarkFlagRequired("token")
 }
