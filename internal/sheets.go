ba97cffef073ef2ee2ce346990e78cba4cc0e1a6
ks6088ts <ks6088ts@gmail.com>
2021-09-06 23:56:02 +0900

    refactor codes

--- internal/sheets.go
+++ internal/sheets.go
@@ -3,20 +3,32 @@ package internal
 import (
 	"context"
 	"errors"
-	"log"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
-func UpdateSheet(filepath, sheetId, text, timestamp string) error {
-	credential := option.WithCredentialsFile(filepath)
-	srv, err := sheets.NewService(context.TODO(), credential)
-	if err != nil {
-		log.Fatal(err)
-		return errors.New("Failed to get service")
-	}
+// SheetClient denotes a Sheets API client
+type SheetClient struct {
+	service *sheets.Service
+	sheetId string
+}
 
+// NewSheetClient create a Sheets API client
+func NewSheetClient(filepath, sheetId string) (*SheetClient, error) {
+	credential := option.WithCredentialsFile(filepath)
+	service, err := sheets.NewService(context.TODO(), credential)
+	if err != nil {
+		return nil, errors.New("Failed to get service")
+	}
+	return &SheetClient{
+		service: service,
+		sheetId: sheetId,
+	}, nil
+}
+
+// AppendValues appends arguments to a sheet
+func (client *SheetClient) AppendValues(text, timestamp string) error {
 	// create ValueRange
 	var row []interface{}
 	var rows [][]interface{}
@@ -26,7 +38,7 @@ func UpdateSheet(filepath, sheetId, text, timestamp string) error {
 	rows = append(rows, row)
 	vr.Values = append(vr.Values, rows...)
 
-	_, err = srv.Spreadsheets.Values.Append(sheetId, "A1", &vr).ValueInputOption("RAW").InsertDataOption("INSERT_ROWS").Do()
+	_, err := client.service.Spreadsheets.Values.Append(client.sheetId, "A1", &vr).ValueInputOption("RAW").InsertDataOption("INSERT_ROWS").Do()
 	if err != nil {
 		return errors.New("Failed to insert rows")
 	}
